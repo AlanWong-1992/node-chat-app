ea548fe15623aec11b2189bd5d1667ece81d8fba
Alan Wong <alan_wong888@yahoo.com>
2018-09-29 15:57:52 +0100

    seperate client side js and added newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'alan',
+    subject: 'Emit new message',
+    createdAt: '123'
+  });
+
+  socket.on('createMessage', (createMessage) => {
+    console.log('create Message', createMessage);
+  })
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
